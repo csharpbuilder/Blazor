b243c2522c7f5354b6031834db1a05893537fd1e
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-06-28 15:19:46 +0100

    Dispatch events back to server

--- src/Microsoft.AspNetCore.Blazor.Browser/Rendering/BrowserRendererEventDispatcher.cs
+++ src/Microsoft.AspNetCore.Blazor.Browser/Rendering/BrowserRendererEventDispatcher.cs
@@ -10,15 +10,15 @@ namespace Microsoft.AspNetCore.Blazor.Browser.Rendering
     /// Provides mechanisms for dispatching events to components in a <see cref="BrowserRenderer"/>.
     /// This is marked 'internal' because it only gets invoked from JS code.
     /// </summary>
-    internal static class BrowserRendererEventDispatcher
+    public static class BrowserRendererEventDispatcher
     {
-        // We receive the information as JSON strings because of current interop limitations:
-        // - Can't pass unboxed value types from JS to .NET (yet all the IDs are ints)
-        // - Can't pass more than 4 args from JS to .NET
-        // This can be simplified in the future when the Mono WASM runtime is enhanced.
-        public static void DispatchEvent(string eventDescriptorJson, string eventArgsJson)
+        /// <summary>
+        /// For framework use only.
+        /// </summary>
+        [JSInvokable(nameof(DispatchEvent))]
+        public static void DispatchEvent(
+            BrowserEventDescriptor eventDescriptor, string eventArgsJson)
         {
-            var eventDescriptor = Json.Deserialize<BrowserEventDescriptor>(eventDescriptorJson);
             var eventArgs = ParseEventArgsJson(eventDescriptor.EventArgsType, eventArgsJson);
             var browserRenderer = BrowserRendererRegistry.Find(eventDescriptor.BrowserRendererId);
             browserRenderer.DispatchBrowserEvent(
@@ -60,11 +60,29 @@ namespace Microsoft.AspNetCore.Blazor.Browser.Rendering
             }
         }
 
-        private class BrowserEventDescriptor
+        /// <summary>
+        /// For framework use only.
+        /// </summary>
+        public class BrowserEventDescriptor
         {
+            /// <summary>
+            /// For framework use only.
+            /// </summary>
             public int BrowserRendererId { get; set; }
+
+            /// <summary>
+            /// For framework use only.
+            /// </summary>
             public int ComponentId { get; set; }
+
+            /// <summary>
+            /// For framework use only.
+            /// </summary>
             public int EventHandlerId { get; set; }
+
+            /// <summary>
+            /// For framework use only.
+            /// </summary>
             public string EventArgsType { get; set; }
         }
     }
